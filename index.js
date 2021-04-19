fe3f41b9c388ae85a49a1a8c7287e9f25c0a37fd
Tareq <tarequl30@gmail.com>
2021-04-19 18:19:31 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const MongoClient = require('mongodb').MongoClient;
+const objectId = require("mongodb").ObjectId
+
+const app = express()
+app.use(bodyParser.json())
+app.use(cors());
+
+const port = process.env.PORT || 5000
+
+const uri = "mongodb+srv://carService:carService79@cluster0.5gken.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("carService").collection("Appointment");
+  const serviceCollection = client.db("carService").collection("service");
+  const reviewsCollection = client.db("carService").collection("reviews");
+  const orderCollection = client.db("carService").collection("orders");
+
+app.post("/addOrder", (req,res)=> {
+    const order = req.body
+    orderCollection.insertOne(order)
+    .then(result => {
+        res.send(result.insertedCount > 0)
+    })
+})
+ app.get("/orders", (req,res)=>{
+    orderCollection.find().toArray((err, docs) => res.send(docs))
+ })
+
+  app.get("/reviews", (req, res) => {
+    reviewsCollection.find().toArray((err, docs) => res.send(docs))
+})
+app.post("/addReview", (req, res) => {
+    const { email, review, name, imgURL } = req.body
+    reviewsCollection.insertOne({
+        name,
+        email,
+        review,
+        imgURL,
+    }).then((result) => res.send(result.insertedCount > 0))
+})
+
+app.get("/services", (req, res) => {
+    serviceCollection.find().toArray((err, docs) => res.send(docs))
+})
+  app.post("/addService", (req,res)=>{
+      const service = req.body
+      serviceCollection.insertOne(service)
+      .then(result => {
+          res.send(result.insertedCount > 0)
+      })
+  })
+
+});
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+  
+  app.listen(process.env.PORT || port)
