f6b69b79d8f16e406d4df81fc7f48d69dbb595f1
Tareq <tarequl30@gmail.com>
2021-04-20 14:51:52 +0600

    update

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ client.connect(err => {
   const serviceCollection = client.db("carService").collection("service");
   const reviewsCollection = client.db("carService").collection("reviews");
   const orderCollection = client.db("carService").collection("orders");
-
 app.post("/addOrder", (req,res)=> {
     const order = req.body
     orderCollection.insertOne(order)
@@ -28,7 +27,6 @@ app.post("/addOrder", (req,res)=> {
  app.get("/orders", (req,res)=>{
     orderCollection.find().toArray((err, docs) => res.send(docs))
  })
-
   app.get("/reviews", (req, res) => {
     reviewsCollection.find().toArray((err, docs) => res.send(docs))
 })
@@ -44,6 +42,11 @@ app.post("/addReview", (req, res) => {
 
 app.get("/services", (req, res) => {
     serviceCollection.find().toArray((err, docs) => res.send(docs))
+})
+app.get("/delete-a-service/:id", (req, res) => {
+    console.log(req.params.id)
+    serviceCollection.deleteOne({_id: objectId(req.params.id)})
+    .then(result => res.send(result.deletedCount > 0))
 })
   app.post("/addService", (req,res)=>{
       const service = req.body
